d64a7948fd689c47bfc4915f1523b7cddf2d7e23
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2021-06-18 16:20:37 +0200

    Moved css/js files in the correct folder

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-$('button').on('click', function() {
-    if ($(this).is('#history')) {
-        $('#history-content').slideToggle().siblings('div').slideUp();
-    } else if ($(this).is('#importance')) {
-        $('#importance-content').slideToggle().siblings('div').slideUp();
-    } else if ($(this).is('#information')) {
-        $('#information-content').slideToggle().siblings('div').slideUp();
-    }
-});
--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,15 @@
 // Initialize Materialize with JQuery
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-  });
+$(document).ready(function() {
+    $('.sidenav').sidenav({ edge: "right" });
+});
+
+// Show/hide content on wikipride.html by clicking on buttons
+$('button').on('click', function() {
+    if ($(this).is('#history')) {
+        $('#history-content').slideToggle().siblings('div').slideUp();
+    } else if ($(this).is('#importance')) {
+        $('#importance-content').slideToggle().siblings('div').slideUp();
+    } else if ($(this).is('#information')) {
+        $('#information-content').slideToggle().siblings('div').slideUp();
+    }
+});
